c51e9fc800c88b89d6d8473b62f03248ef491e61
Samuel <samuelcsalema2@gmail.com>
2025-06-21 13:57:09 -0300

    Caderno 2 Jornalismo de Investigação

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", () => { // Conecta o id do nav com
         }
     });
     // Define a home como padrão
-    showSection("home");
+    showSection("caderno2");
 });
 
 
